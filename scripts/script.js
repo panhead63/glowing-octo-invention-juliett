a2f9c55332d2e1d75361106c7195ab5439a79409
panhead63 <87744145+panhead63@users.noreply.github.com>
2023-11-09 14:31:44 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -8,3 +8,6 @@
 // Well, yesterday I accidentally held down the OK button on a video too long, and a menu popped up! 
 //One of the 4 options was "Not Interested"
 //so I selected it, and the video hasn't been suggested since. 
+
+//cool website
+//https://getpocket.com/explore/item/11-actors-who-asked-for-their-characters-to-be-killed-off?utm_source=pocket-newtab-en-us
