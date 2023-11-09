431494bb9983a92239b203d045932d59605508ca
panhead63 <87744145+panhead63@users.noreply.github.com>
2023-11-09 14:28:45 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,10 @@
 //text placeholder for now 
 
 //random lol
+
+//roku 
+//add a "not interested" option for shows & movies
+
+// Well, yesterday I accidentally held down the OK button on a video too long, and a menu popped up! 
+//One of the 4 options was "Not Interested"
+//so I selected it, and the video hasn't been suggested since. 
