e44cbaaf14e5bba111cd0d35d27c702c78f3537b
panhead63 <87744145+panhead63@users.noreply.github.com>
2023-11-14 12:02:12 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,7 @@
 //text placeholder for now 
 
-//random lol
-
 //roku 
-//add a "not interested" option for shows & movies
-
-// Well, yesterday I accidentally held down the OK button on a video too long, and a menu popped up! 
-//One of the 4 options was "Not Interested"
-//so I selected it, and the video hasn't been suggested since. 
+//add a "not interested" option for shows & movies on roku channel
 
 //cool website
 //https://getpocket.com/explore/item/11-actors-who-asked-for-their-characters-to-be-killed-off?utm_source=pocket-newtab-en-us
