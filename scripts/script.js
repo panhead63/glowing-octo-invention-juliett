28ad7c76f2cf40ad2adaa69e42b40d7772915973
panhead63 <87744145+panhead63@users.noreply.github.com>
2023-11-16 12:04:51 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,10 @@
 //text placeholder for now 
 
-//roku 
-//add a "not interested" option for shows & movies on roku channel
+//craiglist calendar
+//color code months
+//which mo.
+//see future
+//range
 
 //cool website
 //https://getpocket.com/explore/item/11-actors-who-asked-for-their-characters-to-be-killed-off?utm_source=pocket-newtab-en-us
